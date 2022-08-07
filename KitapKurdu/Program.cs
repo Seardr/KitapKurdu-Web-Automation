0ae6019b31f73b401a9914133a469eb9abeaa1a5
Seardr <serdar1998gfb@hotmail.com>
2022-08-07 21:04:54 +0300

    Add project files.

---
+++ KitapKurdu/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using NUnit.Framework;
+
+
+namespace KitapKurdu
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+          // Chrome Tarayıcısı Oluşturma
+            IWebDriver driver = new ChromeDriver();
+
+            // Kitapyurdu Sitesine Giriş
+            SeleniumSetMethods.AnaSayfa(driver, "https://www.kitapyurdu.com/");
+
+            // Arama Çubuğuna "roman" Yazımı ve Aratma
+            SeleniumSetMethods.TextGirisi(driver, "roman", "search_keyword", "Name");
+            SeleniumSetMethods.BtnClick(driver, "#search > span", "CssSelector");
+
+            // Listelenen Romanlar Arasından Rastgele Birinin Sepete Eklenmesi
+            SeleniumSetMethods.Rastgele(driver, "div.product-cr > div.grid_2.alpha.omega.relative > div.hover-menu > a.add-to-cart");
+            var deger = SeleniumSetMethods.VeriKontrol(driver, "/html/body/div[1]/div[3]/div/div[4]/div[1]/div[1]/span", "XPath");
+            if (deger == "1")
+            {
+                Console.WriteLine("Sepette 1 Ürün Bulunmaktadır.");
+                Thread.Sleep(2000);
+            };
+
+            // Sepetim Butonuna Tıklanması
+            SeleniumSetMethods.BtnClick(driver, "/html/body/div[1]/div[3]/div/div[4]/div[1]/div[2]/h4", "XPath");
+
+            // Sepete Git Butonuna Tıklanması
+            SeleniumSetMethods.BtnClick(driver, "/html/body/div[1]/div[3]/div/div[4]/div[2]/div[2]/div[2]/div/a", "XPath");
+
+            // Seçilen Romanın Miktarını 1 Arttırma
+            SeleniumSetMethods.TextGirisi(driver, "2", "#cart_module > div.cart-info > table > tbody > tr > td.quantity > form > input[type=text]:nth-child(1)", "CssSelector");
+            SeleniumSetMethods.BtnClick(driver, "#cart_module > div.cart-info > table > tbody > tr > td.quantity > form > i", "CssSelector");
+
+            // Sepetin Güncellenmesi
+            var guncelleme = SeleniumSetMethods.VeriKontrol(driver, "#swal2-title", "CssSelector");
+
+            if (guncelleme == "Sepetiniz güncelleniyor!")
+            {
+                Console.WriteLine("Sepetimiz Güncellenmiştir!");
+            };
+
+            // Sepetteki Romanın Çıkarılması
+            SeleniumSetMethods.BtnClick(driver, "#cart_module > div.cart-info > table > tbody > tr > td.remove > a", "CssSelector");
+
+            var sepetMiktarı = SeleniumSetMethods.VeriKontrol(driver, "#cart-items-empty", "CssSelector");
+
+            if (sepetMiktarı == "Sepetiniz boş")
+            {
+                Console.WriteLine("Sepetimiz boş durumdadır.");
+                Thread.Sleep(2500);
+            };
+
+            // Tarayıcının Kapatılması
+            SeleniumSetMethods.Close(driver);
+
+        }
+
+
+
+      
+    }
+    
+}   
---
+++ KitapKurdu/SeleniumSetMethods.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using NUnit.Framework;
+
+
+namespace KitapKurdu
+{
+    class SeleniumSetMethods 
+    {
+        
+        public static void AnaSayfa(IWebDriver driver, string url)
+        {
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2500);
+            Console.WriteLine("Kitapyurdu Sitesi Açılmıştır.");
+        }
+
+        // Sorgulanan elemente veri girişi
+        public static void TextGirisi(IWebDriver driver, string text, string element, string elementtype)
+        {
+         
+            if (elementtype == "Name")
+            {
+                IWebElement textSpace = driver.FindElement(By.Name(element));
+                textSpace.Clear();
+                textSpace.SendKeys(text);
+            }
+            if (elementtype == "CssSelector")
+            {
+                IWebElement textSpace = driver.FindElement(By.CssSelector(element));
+                textSpace.Clear();
+                textSpace.SendKeys(text);
+            }
+            if (elementtype == "XPath")
+            {
+                IWebElement textSpace = driver.FindElement(By.XPath(element));
+                textSpace.Clear();
+                textSpace.SendKeys(text);
+            }
+        }
+
+        // Sorgulanan buton elementine tıklanılması
+        public static void BtnClick(IWebDriver driver, string element, string elementtype)
+        {
+            if (elementtype == "Name")
+            {
+                IWebElement btn = driver.FindElement(By.Name(element));
+                btn.Click();
+                Thread.Sleep(2500);
+            }
+            if (elementtype == "CssSelector")
+            {
+                IWebElement btn = driver.FindElement(By.CssSelector(element));
+                btn.Click();
+                Thread.Sleep(2500);
+
+            }
+            if (elementtype == "XPath")
+            {
+                IWebElement btn = driver.FindElement(By.XPath(element));
+                btn.Click();
+                Thread.Sleep(2500);
+
+            }
+
+        }
+
+        // Listelenen Romanlardan Rastgele Seçim
+        public static void Rastgele(IWebDriver driver, string element)
+        {
+            IReadOnlyCollection<IWebElement> productName = driver.FindElements(By.CssSelector(element));
+            Console.Clear();
+            var sayac = 0;
+            List<IWebElement> bookNames = new List<IWebElement>();
+
+            foreach (IWebElement bookName in productName)
+            {
+                bookNames.Add(bookName);
+                sayac++;
+            };
+
+            Random rdm = new Random();
+            int sayi = rdm.Next(sayac);
+
+            bookNames[sayi].Click();
+            Thread.Sleep(2500);
+
+        }
+
+        // Sorgulanan verinin kontrolü
+        public static string VeriKontrol(IWebDriver driver, string element, string elementtype)
+        {
+            if (elementtype == "Name")
+            {
+                string textdeger = driver.FindElement(By.Name(element)).Text;
+                return textdeger;
+            }
+            else if (elementtype == "CssSelector")
+            {
+                string textdeger = driver.FindElement(By.CssSelector(element)).Text;
+                return textdeger;
+            }
+            else if (elementtype == "XPath")
+            {
+                string textdeger = driver.FindElement(By.XPath(element)).Text;
+                return textdeger;
+            }
+            else
+            {
+                return "0";
+            }
+            
+          
+        }
+        // Tarayıcının Kapatılması
+        public static void Close(IWebDriver driver)
+        {
+            driver.Close();
+        }
+
+
+     
+
+
+    }
+}
